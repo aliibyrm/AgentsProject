8626dabc9e1d71414e844d05fbf98b56c853c9ae
aliibyrm <alibayram_02@hotmail.com>
2024-11-29 12:27:52 +0300

    Added: Agent memory

--- main.py
+++ main.py
@@ -3,20 +3,27 @@ from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage
 from langgraph.prebuilt import create_react_agent
+from langgraph.checkpoint.sqlite import SqliteSaver
 
 load_dotenv()
 
 model= ChatOpenAI(model="gpt-4")
+memory = SqliteSaver.from_conn_string(":memory:")
 
 search= TavilySearchResults(max_results=2)
 
 tools=[search]
 
-agent_executor = create_react_agent(model, tools)
+agent_executor = create_react_agent(model, tools,checkpointer=memory)
+
+config = {"configurable": {"thread_id":""
+                                       "123"}}
 
 if __name__ == "__main__":
-    response=agent_executor.invoke(
-        {"messages":[HumanMessage(content="what is the weather in istanbul now?")]}
-    )
-    for r in response["messages"]:
-        print(r.content)
+    while True:
+        user_input = input(">")
+        for chunk in agent_executor.stream(
+                {"messages": [HumanMessage(content=user_input)]}, config
+        ):
+            print(chunk)
+            print("----")
